e2cb3553236fa85ad364e7208f5d4641cb9f8d73
Ahmadreza Hamid Fard <61118112+AhmadrezaHamidi@users.noreply.github.com>
2023-11-07 15:58:08 +0330

    uiyiuyuy

--- ShortLink.Core/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace ShortLink.Core.Models;
-
-public class User : IdentityUser
-{
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-}
--- ShortLink.Service/Interfaces/IRepositoryManager.cs
+++
@@ -1,7 +0,0 @@
-﻿using ShortLink.Service.Interfaces;
-public interface IRepositoryManager
-{
-    ILinkRepository Link { get; }
-    IUserAuthenticationRepository UserAuthentication { get; }
-    Task SaveAsync();
-}
