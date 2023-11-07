0a528be3b9402fb50c6f9f24efdd6380012e3940
Ahmadreza Hamid Fard <61118112+AhmadrezaHamidi@users.noreply.github.com>
2023-11-07 15:58:12 +0330

    7i67

---
+++ Scoring/Domain/Framwork/ISpecification.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Framwork
+{
+    public interface ISpecification<T>
+    {
+        bool IsStatisFieldBy(T value);
+    }
+    public abstract class CompositeSpection<T> : Specification<T>
+    {
+
+        public ISpecification<T> _left { get; private set; }
+        public ISpecification<T> _right { get; private set; }
+
+        //public readonly ISpecification<T> _left;
+        //public readonly ISpecification<T> _right;
+
+        protected CompositeSpection(ISpecification<T> left, ISpecification<T> right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+
+
+
+    }
+
+    public abstract class Specification<T> : ISpecification<T>
+    {
+        public abstract bool IsStatisFieldBy(T value);
+        public Specification<T> And(ISpecification<T> left)
+        {
+            return new AndSpection<T>(this, left);
+        }
+    }
+
+
+    public class OrSpection<T> : CompositeSpection<T>
+    {
+        public OrSpection(ISpecification<T> left, ISpecification<T> right) : base(left, right)
+        {
+        }
+
+        public override bool IsStatisFieldBy(T value)
+        {
+            return _right.IsStatisFieldBy(value) || _left.IsStatisFieldBy(value);
+        }
+    }
+
+
+    public class AndSpection<T> : CompositeSpection<T>
+    {
+        public AndSpection(ISpecification<T> left, ISpecification<T> right) : base(left, right)
+        {
+        }
+
+        public override bool IsStatisFieldBy(T value)
+        {
+            return _right.IsStatisFieldBy(value) && _left.IsStatisFieldBy(value);
+        }
+    }
+}
---
+++ Scoring/Domain/Model/Rules/Rule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Rules
+{
+    public class Rule
+    {
+        public int Id { get;private set; }
+        public string  title { get;private set; }
+
+    }
+
+
+
+    public class CalculationStrategy 
+    {
+         
+    }
+
+
+    public class Citeria
+    {
+
+    }
+}
